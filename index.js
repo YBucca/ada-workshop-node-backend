7dcfa4f1ad02efbecc598a6bfb02ed1550392ed3
Yanina Bucca <yani.bucca@gmail.com>
2021-06-30 23:53:06 +0000

    peliculas frontend y backend

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-const server = express();
-const port = 3000;
+const server = express(); //libreria
+const port = 3000; //puerto de escucha
 
 server.use(cors());
 server.use(express.urlencoded({ extended: true }));
--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const basePath = '/';
+const db = require('./../database.json')
 
 router.get(basePath, (req, res) => {
-    res.status(200).json('Hello World');
+    //filtro de año y tipo de peliculas y se guarda en la const filter
+    const filter = req.query;
+// se aplica el filtro de a los datos que se encuentran en la constante db
+    let result = db.filter(item => {
+        for(let key in filter){
+            if(item[key]=== undefined || item[key] != filter[key])
+            return false;
+        }
+        return true;
+    });
+
+// se procede a realizar la respuesta de la petición http
+    res.status(200).json(result);
 });
 
 module.exports = router;
